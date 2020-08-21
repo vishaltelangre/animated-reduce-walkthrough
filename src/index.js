1072830ce2a6f2bddaff2a6bfe266c8fdb532f74
Vishal Telangre <the@vishaltelangre.com>
2020-08-21 23:50:24 +0530

    Slowed animation

--- src/index.js
+++ src/index.js
@@ -41,14 +41,16 @@ document.addEventListener("DOMContentLoaded", () => {
       initialValue
         .call(activeStyle, initialValue.text())
         .clone(true)
+        .style("opacity", 1)
         .attr("fill", "tomato")
         .attr("font-weight", "bold")
         .attr("dx", -10)
         .transition()
-        .delay(1000)
-        .duration(1000)
+        .delay(2000)
+        .duration(2000)
         .attr("dx", 100)
         .attr("dy", 78)
+        .style("opacity", 0.1)
         .remove();
     } else {
       initialValue.call(resetStyle, initialValue.text());
@@ -67,7 +69,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .end();
 
     await node
-      .call(activeStyle, node.text(), stepNumber * 1000)
+      .call(activeStyle, node.text(), stepNumber * 2000)
       .select(function () {
         return this.parentNode.parentNode;
       })
@@ -77,26 +79,31 @@ document.addEventListener("DOMContentLoaded", () => {
       .attr("fill", "tomato")
       .attr("font-weight", "bold")
       .style("visibility", "hidden")
+      .style("opacity", 1)
       .transition()
       .style("visibility", "visible")
-      .delay(stepNumber * 1000)
-      .duration(1000)
+      .style("opacity", 0.1)
+      .delay(stepNumber * 2000)
+      .duration(2000)
       .attr("x", itemArg1XPos)
       .attr("dy", 38)
       .remove();
   };
 
   const stepToArg = (selection, newText, resetText, stepNumber) => {
-    const elementXPos = (el) => el.node().getBoundingClientRect().left - 104;
-    const sourceXPos = elementXPos(selection);
+    const elementXPos = (el, offset = 104) =>
+      el.node().getBoundingClientRect().x - offset;
+    const sourceXPos = elementXPos(
+      selection,
+      selection === accArg1 ? 104 : 124
+    );
     const destXPos = elementXPos(selection === accArg1 ? accArg2 : itemArg2);
 
     const node = selection
-      .call(activeStyle, newText, stepNumber * 1000)
-      .call(resetStyle, resetText, "skyblue", (stepNumber + 1) * 1000);
+      .call(activeStyle, newText, stepNumber * 2000)
+      .call(resetStyle, resetText, "skyblue", stepNumber * 3000);
 
     if (selection === accArg1 || selection === itemArg1) {
-
       node
         .select(function () {
           return this.parentNode.parentNode;
@@ -107,10 +114,15 @@ document.addEventListener("DOMContentLoaded", () => {
         .attr("fill", "tomato")
         .attr("font-weight", "bold")
         .style("visibility", "hidden")
+        .style("opacity", 0)
         .transition()
-        .style("visibility", "visible")
         .delay(stepNumber * 1000)
-        .duration(1000)
+        .style("opacity", 0.5)
+        .transition()
+        .delay(stepNumber * 1000)
+        .duration(stepNumber * 1000)
+        .style("opacity", 0.1)
+        .style("visibility", "visible")
         .attr("x", destXPos)
         .attr("dy", 58)
         .remove();
@@ -120,23 +132,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const stepToNewAccumulatedValue = (newAcc, stepNumber, isLastIteration) => {
     accEquation
       .transition()
-      .delay(stepNumber * 1000)
-      .duration(isLastIteration ? 3000 : 2000)
+      .delay(stepNumber * 2000)
+      .duration(isLastIteration ? 4000 : 3000)
       .style("visibility", "hidden")
       .transition()
       .style("visibility", "visible");
 
     calculatedAcc
       .transition()
-      .delay(stepNumber * 1000)
-      .duration(1000)
+      .delay(stepNumber * 2000)
+      .style("opacity", 0.5)
+      .duration(isLastIteration ? 1000 : 2000)
+      .style("opacity", 1)
       .style("visibility", "visible")
       .text(newAcc)
       .transition()
+      .duration(isLastIteration ? 3000 : 2000)
       .attr("dx", isLastIteration ? 0 : 166)
       .attr("dy", isLastIteration ? 20 : -60)
       .attr("font-size", isLastIteration ? 80 : 16)
-      .duration(isLastIteration ? 2000 : 1000)
+      .style("opacity", isLastIteration ? 1 : 0.1)
       .transition()
       .attr("dx", 0)
       .attr("dy", 0)
@@ -144,7 +159,7 @@ document.addEventListener("DOMContentLoaded", () => {
       .style("visibility", "hidden");
 
     if (!isLastIteration) {
-      accArg1.call(activeStyle, newAcc, (stepNumber + 2) * 1000);
+      accArg1.call(activeStyle, newAcc, (stepNumber + 2) * 2000);
     }
   };
 
@@ -171,7 +186,7 @@ document.addEventListener("DOMContentLoaded", () => {
       stepToNewAccumulatedValue(accumulatedValue, 4, isLastIteration);
 
       if (isLastIteration) {
-        await sleep(iterationStepCount * 2);
+        await sleep(iterationStepCount * 4);
         step();
       }
     });
